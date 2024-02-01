5191c2f930bd6a561bd2cd86982fe624fc50314c
TheCoder120602 <143860679+TheCoder120602@users.noreply.github.com>
2024-01-31 18:35:48 -0600

    Add files via upload

---
+++ industrial.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef INDUSTRIAL_H
+#define INDUSTRIAL_H
+#include "loc.h"
+#include "residential.h"
+using namespace std;
+
+void updateIndustrial(vector<vector<loc> > &region, int& workers, int& goods);
+
+#endif
