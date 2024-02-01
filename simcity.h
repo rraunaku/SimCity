e0cd645fe8d58ec78a0564fd53a33f528ec9e990
TheCoder120602 <143860679+TheCoder120602@users.noreply.github.com>
2024-01-31 18:37:59 -0600

    Add files via upload

---
+++ simcity.h
@@ -0,0 +1,22 @@
+#pragma once
+#ifndef SIMCITY_H
+#define SIMCITY_H
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include "loc.h"
+#include "residential.h"
+#include "industrial.h"
+#include "commercial.h"
+#include "pollution.h"
+#include "hospital.h"
+using namespace std;
+
+void readInput(string layout, vector<vector<loc>>& region);
+void update(vector <vector<loc>>& region, int& workers, int& goods, bool& same);
+void print(vector<vector<loc>> region);
+
+
+#endif // !READCSV_H
+
