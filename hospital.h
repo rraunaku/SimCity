6e32ffb1bc5bc49cad5baedc117444f8d2dc1ea5
Raunak Upreti <143860679+TheCoder120602@users.noreply.github.com>
2024-02-01 10:36:31 -0600

    Update hospital.h

--- hospital.h
+++ hospital.h
@@ -5,11 +5,11 @@
 #include "residential.h"
 using namespace std;
 
-void createH(vector<vector<loc>>& region);
-void simulateH(vector<vector<loc>>& region, int& goods);
-int findGoods(vector <vector<loc>> region);
-void treat(vector <vector<loc>>& region);
-void distribute(vector <vector<loc>>& region, int inj);
-bool check(vector <vector<loc>>& region);
+void createH(vector<vector<loc> >& region);
+void simulateH(vector<vector<loc> >& region, int& goods);
+int findGoods(vector <vector<loc> > region);
+void treat(vector <vector<loc> >& region);
+void distribute(vector <vector<loc> >& region, int inj);
+bool check(vector <vector<loc> >& region);
 
 #endif
