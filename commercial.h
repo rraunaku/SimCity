2ad2f6880cefb35e8643f96cce48579604eb5bd7
TheCoder120602 <143860679+TheCoder120602@users.noreply.github.com>
2024-01-31 18:34:32 -0600

    Add files via upload

---
+++ commercial.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef COMMERCIAL_H
+#define COMMERCIAL_H
+#include "loc.h"
+#include "residential.h"
+using namespace std;
+
+void updateCommercial(vector<vector<loc>>& region, int& workers, int& goods);
+#endif
